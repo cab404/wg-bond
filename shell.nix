b74cd70533bc3dd70617a1465aea122cedab8ca9
Vladimir Serov <me@cab404.ru>
2020-06-15 19:55:50 +0300

    clippy in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  ruststable = (pkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
+  ruststable = (pkgs.latest.rustChannels.stable.rust.override {
+    extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" "clippy-preview" ];
+  });
 in
 with pkgs; mkShell {
   buildInputs = [ openssl pkgconfig nasm rustup ruststable cmake zlib ];
